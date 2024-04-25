0790524da7129094780d426be0faad04098338e1
Adrian21 <stroescuadrian@gmail.com>
2024-04-25 15:58:46 +0300

    mobile style fix

--- script.js
+++ script.js
@@ -3,17 +3,22 @@
 // Selecting elements
 const score0EL = document.querySelector('#score--0');
 const score1EL = document.getElementById('score--1');
-const diceEL = document.querySelector('.dice');
-const btnNew = document.querySelector('btn--new');
-const btnRoll = document.querySelector('btn--roll');
-const btnHold = document.querySelector('btn--hold');
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
 
 // Starting conditions
 score0EL.textContent = 0;
 score1EL.textContent = 0;
-diceEL.classList.add('hidden');
+diceEl.classList.add('hidden');
 
 // Rolling dice functionality
 btnRoll.addEventListener('click', function () {
+  // 1. Generating a random dice roll
   const dice = Math.trunc(Math.random() * 6) + 1;
+
+  // 2. Display dice
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${dice}.png`;
 });
